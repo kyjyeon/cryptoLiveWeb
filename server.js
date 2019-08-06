b06e1d2dc229929efeb7d83bac7a9e1f046f3a96
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-08-07 00:24:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,7 @@ const PORT = process.env.PORT || 5000;
 //BodyParser Middleware
 app.use(bodyParser.json());
 
-// app.get("/api/table", (req,res,next)=>{
-//     res.send({})
-// })
-
-//Use Routes
+//Use Routes, receive data form DB
 app.use('/api/items', items);
 
 app.listen(PORT, ()=>console.log(`Server listening on PORT ${PORT}`));
