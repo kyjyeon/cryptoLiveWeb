8cf34c2ff8bcec7250a543d6134bd937ec70364f
YJ Kim <39256000+kyjyeon@users.noreply.github.com>
2019-08-29 15:44:17 +0900

    Update coin.js

--- coin.js
+++ coin.js
@@ -17,7 +17,7 @@ EUR = "eur";
 var connection = mysql.createConnection({
   host: "kyjdb.cdzelssaqpcy.ap-northeast-2.rds.amazonaws.com",
   user: "kyjyeon",
-  password: "dus900809!",
+  password: "",
   database: "coin",
   port: 3400
 })
